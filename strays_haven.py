f51f7c2648130921a7164dcad03c015a2037da8e
Ayim William <odhisayim@gmail.com>
2025-05-15 10:13:51 +0300

    added class attributes

--- strays_haven.py
+++ strays_haven.py
@@ -11,8 +11,11 @@ print(Rasmus.speak())
 
 #A simple class to represent a dog with modified __init__   
 class Dog:
+
+    species = "canis lupus familiaris" # class attribute
+
     def __init__(self,name,breed,age="N/A"):
-        self.name = name
+        self.name = name # instance attribute
         self.breed = breed
         self.age = age
 
@@ -20,11 +23,17 @@ class Dog:
         return f"{self.name} says woof! woof!"
     
 koba = Dog("Koba","Great Dane", 3)
-amad = Dog("amad", "Black Goat")
+amad = Dog("amad", "Black Goat") # works because we intialised age with a default value
 koba.age = 4
 print(koba.speak())
 print(koba.age)
 print(amad.age)
+print(amad.species)
+print(amad.name)
+
+print(koba.species)
+print(koba.name)
+
 
 class Cat:
     pass
